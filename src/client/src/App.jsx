4fc47e8c3b01a1109a1322b67095ea95a2ff2341
Shubhansu Singh Bhadoria <shubhansusb90@gmail.com>
2024-08-23 01:03:40 +0530

    Include dist directory in deployment

--- src/client/src/App.jsx
+++ src/client/src/App.jsx
@@ -1,6 +1,38 @@
+// import React from 'react';
+// import Navbar from './components/Navbar';
+// // import WelcomeSection from './components/WelcomeSection';
+// import OurOfferings from './components/OurOfferings';
+// import ContactUs from './components/ContactUs';
+// import backgroundImage from './assets/79compressed.jpg';
+// import Testimonials from './components/Testimonials';
+// import CompanyIntroCard from './components/CompanyIntroCard';
+
+// const App = () => {
+//   return (
+//     <div className="min-h-screen overflow-x-hidden">
+//       <Navbar />
+//       <div className="relative w-full h-screen overflow-hidden">
+//         <img src={backgroundImage} alt="Background" className="absolute inset-0 w-full h-full object-cover" />
+//         <CompanyIntroCard />
+//       </div>
+//       <div id="ourofferings">
+//         <OurOfferings />
+//       </div>
+//       <div id='testimonials'>
+//         <Testimonials />
+//       </div>
+//       <div id="contactus">
+//         <ContactUs />
+//       </div>
+//     </div>
+//   );
+// }
+
+// export default App;
+
+
 import React from 'react';
 import Navbar from './components/Navbar';
-// import WelcomeSection from './components/WelcomeSection';
 import OurOfferings from './components/OurOfferings';
 import ContactUs from './components/ContactUs';
 import backgroundImage from './assets/79compressed.jpg';
@@ -11,8 +43,14 @@ const App = () => {
   return (
     <div className="min-h-screen overflow-x-hidden">
       <Navbar />
-      <div className="relative w-full h-screen overflow-hidden">
-        <img src={backgroundImage} alt="Background" className="absolute inset-0 w-full h-full object-cover" />
+      <div className="relative w-full h-screen overflow-hidden bg-gray-200"> {/* Add a placeholder color */}
+        <img
+          src={backgroundImage}
+          alt="Background"
+          className="absolute inset-0 w-full h-full object-cover"
+          loading="eager" // Ensure the image loads quickly
+          style={{ display: 'block' }} // Prevent layout shifts
+        />
         <CompanyIntroCard />
       </div>
       <div id="ourofferings">
