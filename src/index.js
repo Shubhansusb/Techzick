a34d494854af9b59734c7e252d7d7b851f8ec722
Shubhansu Singh Bhadoria <shubhansusb90@gmail.com>
2024-08-23 00:34:39 +0530

    Fix backend serving frontend issue and update configuration

--- src/index.js
+++ src/index.js
@@ -1,28 +1,58 @@
+// require('dotenv').config();
+// const express = require('express');
+// const cors = require('cors');
+// const bodyparser = require('body-parser');
+// const path = require('path');
+// const app = express();
+// app.use(cors());
+// app.use(bodyparser.json());
+// const router = require('./routes');
+
+// app.use('/api', router);
+
+// app.use(express.json());
+// app.use(express.urlencoded({ extended: true }));
+
+// app.get('/', (req, res) => {
+// 	res.status(200).send('Server is up and running');
+// });
+// app.use(express.static(path.join(__dirname, '/src/client', 'build')));
+// app.get('*', (req, res) => {
+// 	res.sendFile(path.resolve(__dirname, '/src/client', 'build', 'index.html'));
+//   });
+// const port = process.env.PORT || 8000;
+// app.listen(port, () => {
+// 	console.log(`Server is running on port ${port}`);
+// });
+
+// module.exports = app;
+
+
 require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
-const bodyparser = require('body-parser');
 const path = require('path');
 const app = express();
+
 app.use(cors());
-app.use(bodyparser.json());
-const router = require('./routes');
-
-app.use('/api', router);
-
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.get('/', (req, res) => {
-	res.status(200).send('Server is up and running');
-});
-app.use(express.static(path.join(__dirname, '/src/client', 'build')));
+const router = require('./routes');
+app.use('/api', router);
+
+// Serve static files from the React app
+app.use(express.static(path.join(__dirname, 'src/client/build')));
+
+// Catch-all handler for any request that doesn't match an API route
 app.get('*', (req, res) => {
-	res.sendFile(path.resolve(__dirname, '/src/client', 'build', 'index.html'));
-  });
+  res.sendFile(path.join(__dirname, 'src/client/build', 'index.html'));
+});
+
+// Start the server
 const port = process.env.PORT || 8000;
 app.listen(port, () => {
-	console.log(`Server is running on port ${port}`);
+  console.log(`Server is running on port ${port}`);
 });
 
 module.exports = app;
