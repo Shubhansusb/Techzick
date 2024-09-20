37e5311ae93b6502771b1b56430769afe4417d42
Shubhansu Singh Bhadoria <shubhansusb90@gmail.com>
2024-09-20 07:48:36 +0530

    adding the animation to the bg i

--- src/client/src/App.jsx
+++ src/client/src/App.jsx
@@ -1,64 +1,3 @@
-// import React from 'react';
-// import Navbar from './components/Navbar';
-// import OurOfferings from './components/OurOfferings';
-// import ContactUs from './components/ContactUs';
-// import backgroundImage from './assets/robo.png';
-// import Testimonials from './components/Testimonials';
-// import CompanyIntroCard from './components/CompanyIntroCard';
-// import './App.css';
-
-// const App = () => {
-//   return (
-//     <div className="">
-//       <Navbar />
-//       <div className="relative w-full h-screen overflow-hidden bg-gray-200">
-//         {/* <link rel="preload" href={backgroundImage} as="image" /> */}
-
-//         <div className="absolute inset-0 w-full h-full bg-gray-300"></div>
-
-//         {/* <img
-//           src={backgroundImage}
-//           alt="Background"
-//           className="absolute inset-0 w-full h-full object-cover"
-//           loading="eager"
-//           style={{ opacity: 0, transition: 'opacity 1s ease-in-out' }}
-//           onLoad={(e) => e.target.style.opacity = 1}
-//         /> */}
-
-//         <div id='companyintro'>
-//           <CompanyIntroCard />
-//         </div>
-//       </div>
-//       <div id="ourofferings">
-//         <OurOfferings />
-//       </div>
-//       <div id='testimonials'>
-//         <Testimonials />
-//       </div>
-//       <div id="contactus">
-//         <ContactUs />
-//       </div>
-//     </div>
-//   );
-// }
-
-// export default App;
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 import React from 'react';
 import Navbar from './components/Navbar';
@@ -71,13 +10,10 @@ import './App.css';
 
 const App = () => {
 	return (
-    <div className='app-container'>
-      
+		<div className='app-container'>
 			<Navbar />
-      <CompanyIntroCard/>
-			<div className='background-container'>
-				
-			</div>
+			<CompanyIntroCard />
+			<div className='background-container'></div>
 
 			<div id='ourofferings'>
 				<OurOfferings />
@@ -93,17 +29,6 @@ const App = () => {
 };
 
 export default App;
-
-
-
-
-
-
-
-
-
-
-
 
 
 
